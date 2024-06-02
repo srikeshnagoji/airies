e4788ea0188b59b11ed8bfc1c818f996e3b143af
srikeshnagoji <36105652+srikeshnagoji@users.noreply.github.com>
2024-06-02 22:09:22 +0530

    Added floating nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { CardHoverEffectDemo } from "@/components/ui/CardHoverEffectDemo";
+import { FloatingNavDemo } from "@/components/ui/FloatingNavDemo";
 import { SparklesPreview } from "@/components/ui/SparklesPreview";
 import { VortexDemo } from "@/components/ui/VortexDemo";
 import { WavyBackgroundDemo } from "@/components/ui/WavyBackgroundDemo";
@@ -8,6 +9,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="h-screen">
+      <FloatingNavDemo/>
     <SparklesPreview />
     {/* <WavyBackgroundDemo/> */}
     <VortexDemo/>
